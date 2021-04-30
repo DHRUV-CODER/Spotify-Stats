6b970bdf4d8122c66dc3ed12a4cf0630e15c06c8
Adith625 <adith031525@gmail.com>
2021-04-30 23:24:05 +0530

    ✨auto refresh when song changes

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import time
 import requests
 from flask import Flask, render_template
+from environs import Env
+import json
+env = Env()
 app = Flask(__name__)
-
 current_track_info = "https://api.spotify.com/v1/me/player/currently-playing"
-auth = "" #Go here and get your token -> https://developer.spotify.com/console/get-users-currently-playing-track/?market=&additional_types=
-
+auth = env.str("auth") #Go here and get your token -> https://developer.spotify.com/console/get-users-currently-playing-track/
+#		$export auth=<key>
+current_music="{}"
+current_music=json.loads(current_music)
 @app.route('/')
 def spotify_reg():
+    global current_music
     res = requests.get(current_track_info, params={'market': 'ES'}, headers={
         "Authorization": f"Bearer {auth}"}).json()
     current_music = res['item']['album']['name']
@@ -15,9 +20,18 @@ def spotify_reg():
     Img = res['item']['album']['images'][1]['url']
     return render_template('stats.html', cm=current_music, author=author, img=Img)
 
-    
+
+@app.route('/state')
+def check():
+    res = requests.get(current_track_info, params={'market': 'ES'}, headers={"Authorization": f"Bearer {auth}"}).json()
+    cm = res['item']['album']['name']
+    if cm!=current_music:
+        print("song changed")
+        resp='''{"state":"changed"}'''
+    else:
+        resp='''{"state":"same"}'''
+    return resp
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
