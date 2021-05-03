e841f24a14dadd0bce50cf13ec0444534f04d63f
Dhruv <60794694+DHRUV-CODER@users.noreply.github.com>
2021-05-03 11:16:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import time
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import json
+import requests as r
 app = Flask(__name__)
 
-app.secret_key = "something-random"
-app.config['SESSION_COOKIE_NAME'] = "Dhruv's Cookie"
+app.secret_key = ""
+app.config['SESSION_COOKIE_NAME'] = "Dhruv's Cookie"  
 TOKEN_INFO = "token-info"
 Current_Music="{}"
 Current_Music=json.loads(Current_Music)
@@ -25,8 +26,7 @@ def redirectPage():
     code = request.args.get('code')
     token_info = sp_oauth.get_access_token(code)
     session[TOKEN_INFO] = token_info
-    return redirect(url_for('spotify_reg', _external=True))
-
+    return redirect(url_for('spotify_reg', _external=True,_scheme='https'))
 
 
 
@@ -48,13 +48,15 @@ def spotify_reg():
         token_info = get_token()
     except:
         print("Not Logged In")
-        return redirect('/')
+        return redirect(url_for('login' , _external=False ,_scheme='https'))
     sp = spotipy.Spotify(auth=token_info['access_token'])
     res = sp.currently_playing()
     Current_Music = res['item']['album']['name']
     Music_Link = res['item']['album']['external_urls']['spotify']
     Author_of_music = res['item']['artists'][0]['name']
     Image_Preview_of_Song = res['item']['album']['images'][1]['url']
+    lyrics = r.get(f'https://some-random-api.ml/lyrics?title={Current_Music}').json()
+    final_lyrics = lyrics['lyrics']
     return render_template('stats.html', cm=Current_Music, author=Author_of_music, img=Image_Preview_of_Song,link = Music_Link)
 
 @app.route('/state')
@@ -63,7 +65,7 @@ def check():
         token_info = get_token()
     except:
         print("Not Logged In")
-        return redirect('/')
+        return redirect(url_for('login', _external=False,_scheme='https'))
     sp = spotipy.Spotify(auth=token_info['access_token'])
     res = sp.currently_playing()
     cm = res['item']['album']['name']
@@ -75,11 +77,16 @@ def check():
     return resp
 
 
+@app.errorhandler(500)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return "<center><p>Play Song or If AD is going then skip it</p></center>", 404
+
 def create_spotify_oauth():
     return SpotifyOAuth(
-        client_id="client_id",
-        client_secret="client_secreat",
-        redirect_uri=url_for('redirectPage', _external=True),
+        client_id="",
+        client_secret="",
+        redirect_uri=url_for('redirectPage', _external=True,_scheme='https'),
         scope="user-read-currently-playing")
 
 
